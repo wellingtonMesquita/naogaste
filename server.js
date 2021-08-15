fae8cdcf3d1b26e4f79508bd02b9e0c712e9ddf7
wellingtonMesquita <wellington88tads@gmail.com>
2021-08-14 22:06:27 -0300

    teste

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.get("/*", function (req, res) {
 
 // Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
